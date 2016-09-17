d9baba80ed5a451370c9af4a068459fcfd82669f
Manos Pappas <mano.pappa@gmail.com>
2016-09-17 21:08:57 +0300

    New start commit_Node Client with Node.js and Express serving admin panel Web Site

---
+++ README.md
@@ -0,0 +1,19 @@
+# Client Web Application with Node.js and Express.js.
+
+Rendering static html files using Express web framework.
+
+## Built With
+
+* Node.js
+* Express.js
+
+
+## Getting Started
+
+### Installing
+
+A step by step series of examples
+
+```
+
+```
---
+++ postmethod.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+
+app.use('/static', express.static(path.join(__dirname + '/public')));
+
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+
+    // Prepare output in JSON format
+    response = {
+        first_name:req.body.first_name,
+        last_name:req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
+
+
+
+var server = app.listen(8081, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+
+});
